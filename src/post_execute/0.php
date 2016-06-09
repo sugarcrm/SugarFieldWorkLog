b59c6898fbc4483d27aa611b7f202443663960c9
Jerry Clark <geraldbclark@gmail.com>
2016-06-09 01:28:20 -0400

    Update 0.php

--- src/post_execute/0.php
+++ src/post_execute/0.php
@@ -3,12 +3,11 @@
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $output = true;
-if (php_sapi_name() === 'cli')
-{
+if (php_sapi_name() === 'cli') {
      $output = false;
 }
 
 require_once('modules/Administration/QuickRepairAndRebuild.php');
 $RAC = new RepairAndClear();
 $actions = array('clearAll');
-$RAC->repairAndClearAll($actions, array(translate('LBL_ALL_MODULES')), false, $output);
+$RAC->repairAndClearAll($actions, array('All Modules'), false, $output);
